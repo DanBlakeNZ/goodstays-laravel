04eb9af7c4f7f5cf0e828a7c0d095bf8964f568d
Dan <dblakenz@hotmail.com>
2020-02-20 12:06:27 +1100

    added into text to hero

--- resources/views/includes/hero.blade.php
+++ resources/views/includes/hero.blade.php
@@ -1,3 +1,14 @@
 <div class="hero hero--full">
-  HERO
+  <div class="hero__inner">
+    <div class="hero__content">
+      <div class="hero__wrap wrap--narrow">
+        <div class="hero__intro">
+          <h1>
+            <img src="https://res.cloudinary.com/dblakenzcloud/image/upload/v1582158469/Goodstays/home-intro.png" alt="Your Good Holiday Starts Here">
+          </h1>
+          <p>Explore our extensive selection of accommodation for holidays and short stays in <a href="/accommodation/queenstown/">Queenstown</a>, <a href="/accommodation/wanaka/">Wanaka</a>, <a href="/accommodation/jacks-point/">Jack's Point</a> &amp; <a href="/accommodation/lake-hawea-accommodation/">Lake Hawea</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
